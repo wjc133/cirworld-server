affac28fabe6228e62936df36e1bffa93a61e1cf
wjc133 <wjc133@sina.com>
2016-02-02 01:27:55 +0800

    init commit

---
+++ cirworld-admin/src/test/java/com/elite/cirworld/admin/BaseControllerTest.java
@@ -0,0 +1,9 @@
+package com.elite.cirworld.admin;
+
+/**
+ * Created by wjc133
+ * Date: 2016/2/2
+ * Time: 1:20
+ */
+public class BaseControllerTest {
+}
---
+++ cirworld-share/src/main/java/com/elite/cirworld/share/BaseController.java
@@ -0,0 +1,9 @@
+package com.elite.cirworld.share;
+
+/**
+ * Created by wjc133
+ * Date: 2016/2/2
+ * Time: 1:20
+ */
+public class BaseController {
+}
