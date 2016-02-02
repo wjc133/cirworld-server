411652f7e649a6ee543d88a37fde03b532b0d194
wjc133 <wjc133@sina.com>
2016-02-02 22:09:21 +0800

    add controller

---
+++ cirworld-web/src/main/java/com/elite/cirworld/web/controller/CyclingRecordController.java
@@ -0,0 +1,14 @@
+package com.elite.cirworld.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by wjc133
+ * Date: 2016/2/2
+ * Time: 22:08
+ */
+@Controller
+@RequestMapping("cir")
+public class CyclingRecordController {
+}
---
+++ cirworld-web/src/main/java/com/elite/cirworld/web/controller/PlanController.java
@@ -0,0 +1,15 @@
+package com.elite.cirworld.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by wjc133
+ * Date: 2016/2/2
+ * Time: 21:21
+ */
+@Controller
+@RequestMapping("cir")
+public class PlanController {
+
+}
